bd2edf42e5ea506de7dfb0e7e09d13b9e09cbe68
amol3ng <amohelamokhele@gmail.com>
2025-10-31 11:39:18 +0200

    Intro to JS arrays

---
+++ arrays.js
@@ -0,0 +1,9 @@
+const colors = [];
+colors.push('purple', 'red', 'pink');
+
+const numbers = [2,4,6,8,10];
+colors.pop();
+colors.unshift('yellow');
+
+console.log('Colors array length:', colors.length);
+console.log('Numbers array length:', numbers.length);
